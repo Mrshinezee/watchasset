9a857e113017948db8cdb553e3f315ce494251de
Mrshinezee <ijeomaharinze@gmail.com>
2020-01-30 15:10:07 +0100

    setting up redux on the application

---
+++ src/redux/store.js
@@ -0,0 +1,44 @@
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reduxThunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from './reducer/index';
+
+const saveState = (state) => {
+  try {
+    localStorage.setItem('state', JSON.stringify(state));
+    return undefined;
+  } catch (e) {
+    return undefined;
+  }
+};
+
+const loadState = () => {
+  try {
+    const state = localStorage.getItem('state');
+    if (state === null) {
+      return undefined;
+    }
+    return JSON.parse(state);
+  } catch (e) {
+    return undefined;
+  }
+};
+
+const middlewares = [reduxThunk];
+const persistedState = loadState();
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
+
+const store = createStore(
+  rootReducer,
+  persistedState,
+  composeWithDevTools(applyMiddleware(...middlewares)),
+);
+
+store.subscribe(() => {
+  saveState(store.getState());
+});
+
+export default store;
